d527d24e42476624f1bf3bbcb3ad2afb6c1d6beb
mrin2810 <mrinmayeerane2810@gmail.com>
2022-02-01 01:37:08 +0530

    making code ready for Week 1 - Video 2

--- src/index.js
+++ src/index.js
@@ -1,27 +1,16 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-function Person(props) {
-    function handlePersonClick() {
-        alert(props.person);
-        console.log(props.person);
-    }
-    return <li onClick={handlePersonClick}>{props.person}</li>
-}
-
 function App() {
-    const people = ["Jack", "Judy", "Monica", "Ross"];
-
     function handleInputChange(event) {
         const inputValue = event.target.value; 
         console.log(inputValue);
     }
 
     return (
-        <ul>
-            {people.map((person, i) => <Person person={person} key={i} />)}
+        <div>
             <input onChange={handleInputChange}/>
-        </ul>
+        </div>
     )
 }
 
