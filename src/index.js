e186869e9d72f55f910dd697980c4b8519287be2
mrin2810 <mrinmayeerane2810@gmail.com>
2022-02-02 01:19:38 +0530

    Week 1 Video 2 - Code

--- src/index.js
+++ src/index.js
@@ -1,25 +1,46 @@
-import React from "react";
+import React, { useRef } from "react";
 import ReactDom from "react-dom";
 import { useEffect, useState } from "react/cjs/react.development";
 
-const endpoint = 'https://api.github.com/users/mrin2810';
 function App() {
-
+    
     const [user, setUser] = useState(null);
+    const [username, setUsername] = useState('codeartistryio');
+    const searchInput = useRef();
 
     useEffect(() => {
-        async function getUser() {
-            const response = await fetch(endpoint);
-            const data = await response.json();
-            setUser(data);
-        }
         getUser();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
+    
+    async function getUser() {
+        const baseURL = 'https://api.github.com/users/';
+        const response = await fetch(`${baseURL}${username}`);
+        const data = await response.json();
+        setUser(data);
+    }
+
+    function handleClearClick() {
+        searchInput.current.value = "";
+        searchInput.current.focus();
+    }
 
     return (
         <>
-            <div>{user && user.name}</div>
-            <div>{user && user.html_url}</div>
+            <input
+                type="text"
+                onChange={(event) => setUsername(event.target.value)}
+                ref={searchInput}
+                placeholder="Input username"
+            />
+            <button onClick={handleClearClick}>Clear</button>
+            <div>
+                <p>{user && user.name}</p>
+                <p>{user && user.login}</p>
+            </div>
+            <button
+                onClick={getUser}
+            >Fetch User from Github</button>
         </>
     )
 }
