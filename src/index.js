a0761e1a89f718bbe827083f5da5dd1124010a1f
mrin2810 <mrinmayeerane2810@gmail.com>
2022-02-01 20:23:51 +0530

    Added Title

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 import React, { useState } from "react";
 import ReactDom from "react-dom";
+import { useEffect } from "react/cjs/react.development";
 
 function App() {
     const [developer, setDeveloper] = useState({
+        name: "",
         lang: 'python',
         yoe: 0,
+        isEmployed: false
     });
+
+    useEffect(() => {
+        document.title = developer.name !== "" ? developer.name : "User";
+    }, [developer.name]);
 
     return (
         <>
+            <div><button onClick={() => {
+                setDeveloper({
+                    ...developer,
+                    isEmployed: !developer.isEmployed,
+                })
+            }}>Toggle Employment</button></div>
             <div>
                 <button onClick={() => setDeveloper({
                     lang: 'JS',
@@ -16,7 +29,16 @@ function App() {
                 })}>Change Language</button>
             </div>
             <p>
-                <input
+                Name: <input
+                    type="text"
+                    onChange={(event) => setDeveloper({
+                        ...developer,
+                        name: event.target.value
+                    })}
+                />
+            </p>
+            <p>
+                Years of Experience: <input
                     type="number"
                     onChange={(event) => setDeveloper({
                         ...developer,
@@ -24,8 +46,10 @@ function App() {
                     })}
                 />
             </p>
+            {developer.name !== "" && <p>Hello! {developer.name} here.</p>}
             <p>I am learning {developer.lang}.</p>
             <p>I have {developer.yoe} years of Experience. </p>
+            <p>I am {!developer.isEmployed && <b>{'not '}</b>}<b>employed</b> currently. </p>
         </>
     )
 }
