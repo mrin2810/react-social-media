779c1634b779a1ab2db634cc1a7f916153e96c4c
mrin2810 <mrinmayeerane2810@gmail.com>
2022-02-02 01:24:41 +0530

    Start of Week 1 Video 3

--- src/index.js
+++ src/index.js
@@ -1,46 +1,10 @@
-import React, { useRef } from "react";
+import React from "react";
 import ReactDom from "react-dom";
-import { useEffect, useState } from "react/cjs/react.development";
 
 function App() {
-    
-    const [user, setUser] = useState(null);
-    const [username, setUsername] = useState('codeartistryio');
-    const searchInput = useRef();
-
-    useEffect(() => {
-        getUser();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-    
-    async function getUser() {
-        const baseURL = 'https://api.github.com/users/';
-        const response = await fetch(`${baseURL}${username}`);
-        const data = await response.json();
-        setUser(data);
-    }
-
-    function handleClearClick() {
-        searchInput.current.value = "";
-        searchInput.current.focus();
-    }
-
     return (
         <>
-            <input
-                type="text"
-                onChange={(event) => setUsername(event.target.value)}
-                ref={searchInput}
-                placeholder="Input username"
-            />
-            <button onClick={handleClearClick}>Clear</button>
-            <div>
-                <p>{user && user.name}</p>
-                <p>{user && user.login}</p>
-            </div>
-            <button
-                onClick={getUser}
-            >Fetch User from Github</button>
+            <div>App</div>
         </>
     )
 }
