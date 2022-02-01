f22d3e86c1772d3c41ea05686d61e93b12b3d01b
mrin2810 <mrinmayeerane2810@gmail.com>
2022-02-01 20:39:22 +0530

    mousePosition Displayed

--- src/index.js
+++ src/index.js
@@ -3,54 +3,26 @@ import ReactDom from "react-dom";
 import { useEffect } from "react/cjs/react.development";
 
 function App() {
-    const [developer, setDeveloper] = useState({
-        name: "",
-        lang: 'python',
-        yoe: 0,
-        isEmployed: false
+    const [mousePosition, setMousePosition] = useState({
+        x: 0,
+        y: 0,
     });
 
-    useEffect(() => {
-        document.title = developer.name !== "" ? developer.name : "User";
-    }, [developer.name]);
+    function handleMouseMove(event) {
+        setMousePosition({
+            x: event.pageX,
+            y: event.pageY,
+        });
+    }
 
+    useEffect(() => {
+        document.addEventListener('mousemove', handleMouseMove);
+    }, [])
     return (
-        <>
-            <div><button onClick={() => {
-                setDeveloper({
-                    ...developer,
-                    isEmployed: !developer.isEmployed,
-                })
-            }}>Toggle Employment</button></div>
-            <div>
-                <button onClick={() => setDeveloper({
-                    lang: 'JS',
-                    yoe: 2
-                })}>Change Language</button>
-            </div>
-            <p>
-                Name: <input
-                    type="text"
-                    onChange={(event) => setDeveloper({
-                        ...developer,
-                        name: event.target.value
-                    })}
-                />
-            </p>
-            <p>
-                Years of Experience: <input
-                    type="number"
-                    onChange={(event) => setDeveloper({
-                        ...developer,
-                        yoe: event.target.value
-                    })}
-                />
-            </p>
-            {developer.name !== "" && <p>Hello! {developer.name} here.</p>}
-            <p>I am learning {developer.lang}.</p>
-            <p>I have {developer.yoe} years of Experience. </p>
-            <p>I am {!developer.isEmployed && <b>{'not '}</b>}<b>employed</b> currently. </p>
-        </>
+        <div>
+            <p>x-coordinate: {mousePosition.x}</p>
+            <p>y-coordinate: {mousePosition.y}</p>
+        </div>
     )
 }
 
