c60f07d85f9832b484f4fa827198487a6bd0ff91
mrin2810 <mrinmayeerane2810@gmail.com>
2022-02-02 13:27:36 +0530

    Created Create Post Component

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,7 +1,26 @@
 import React from "react";
 
 function CreatePost() {
-    return <div>Create Post</div>
+    function handleSubmit(event) {
+        event.preventDefault();
+    }
+    return (
+        <>
+            <div>
+                <h1>Create New Post</h1>
+                <form onSubmit={handleSubmit}>
+                    <input 
+                        type="text"
+                        placeholder="Add Post Content"
+                    />
+                    <input
+                        type="file"
+                    />
+                    <button type="submit">Submit Post</button>
+                </form>
+            </div>
+        </>
+    )
 }
 
 export default CreatePost;
