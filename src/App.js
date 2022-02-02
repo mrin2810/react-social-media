e69e5ceb96c81a99d646f1dc6c77d730905bec1a
mrin2810 <mrinmayeerane2810@gmail.com>
2022-02-02 14:09:45 +0530

    display posts and clear input for text, file types

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import CreatePost from './components/CreatePost';
 
 function App() {
     const [user, setUser] = useState();
+    const [posts, setPosts] = useState([]);
 
     useEffect(() => {
         document.title = user ? `${user}'s Feed` : 'Please Login';
@@ -14,11 +15,21 @@ function App() {
     }, [user]);
 
     if(!user) {
-        return <Login setUser={setUser}/>
+        return <Login setUser={setUser} />
     }
     return (<>
-        <Header user={user} setUser={setUser}/>
-        <CreatePost />
+        <Header user={user} setUser={setUser} />
+        <CreatePost user={user} posts={posts} setPosts={setPosts} />
+        {posts && posts.map((post, i) => (
+            <React.Fragment key={i}>
+                {post.content && <p>{post.content}</p>}
+                {post.image && <img 
+                    src={URL.createObjectURL(post.image)} 
+                    style={{ height: 100, width: 200, objectFit: 'cover'}} 
+                    alt='Post Cover'
+                />}
+            </React.Fragment>
+        ))}
     </>)
 }
 
--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,16 +1,18 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 function CreatePost({ user, posts, setPosts }) {
     const [content, setContent] = useState('');
     const [image, setImage] = useState(null);
-
-
+    const imageInputRef = useRef();
     function handleSubmit(event) {
         event.preventDefault();
         const post = { content, image, user };
         const newPosts = [post, ...posts];
         setPosts(newPosts);
+        setContent('');
+        imageInputRef.current.value = '';
     }
+
     return (
         <>
             <div>
@@ -20,10 +22,12 @@ function CreatePost({ user, posts, setPosts }) {
                         type="text"
                         placeholder="Add Post Content"
                         onChange={(event) => setContent(event.target.value)}
+                        value={content}
                     />
                     <input
                         type="file"
                         onChange={(event) => setImage(event.target.files[0])}
+                        ref={imageInputRef}
                     />
                     <button type="submit">Submit Post</button>
                 </form>
