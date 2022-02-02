b94ba2439b70aa8f4263a0f3e335771a12fc49fe
mrin2810 <mrinmayeerane2810@gmail.com>
2022-02-02 13:23:19 +0530

    added header structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React, { useState } from 'react';
+import { useEffect } from 'react/cjs/react.development';
 
 import Login from './components/Login';
+import Header from './components/Header';
+import CreatePost from './components/CreatePost';
 
 function App() {
     const [user, setUser] = useState();
+
+    useEffect(() => {
+        document.title = user ? `${user}'s Feed` : 'Please Login';
+
+    }, [user]);
+
     if(!user) {
         return <Login setUser={setUser}/>
     }
-    return <div>App</div>
+    return (<>
+        <Header user={user} setUser={setUser}/>
+        <CreatePost />
+    </>)
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,12 @@
 import React from "react";
 
-function Header() {
-    return <div>Header</div>
+function Header({ user, setUser }) {
+    return (
+        <>
+            <h2>Welcome {user}!</h2>
+            <button onClick={() => setUser('')}>Logout</button>
+        </>
+    );
 }
 
 export default Header;
